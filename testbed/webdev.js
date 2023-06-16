6741e73cef902c273f238c7a7cb0159f718b0527
SeviGouland <sevigouland@gmail.com>
2023-06-16 21:45:59 +0300

    add DropDownMenu and ToggleButtonMenu

---
+++ testbed/webdev.js
@@ -0,0 +1,10 @@
+function toggleButtonHandler() {
+    
+    let toggleMenu = document.querySelector('.toggle-menu');
+    let toggleMenuDisplayStatus = window.getComputedStyle(toggleMenu).getPropertyValue('display');
+
+    // if ()
+
+
+
+}
